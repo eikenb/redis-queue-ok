22bb110dcdc974efc7c3b115f1542c16474939e8
John Eikenberry <jae@zhar.net>
2012-01-27 16:37:23 -0800

    switch to default gofmt formatting

--- populate.go
+++ populate.go
@@ -6,31 +6,31 @@ It currently just uses json blobs hardcoded below.
 package main
 
 import (
-    "tideland-rdc.googlecode.com/hg"
-    "fmt"
-    "flag"
+	"tideland-rdc.googlecode.com/hg"
+	"fmt"
+	"flag"
 )
 
 func main() {
-    blobs := []string{
-        `{ "firstname":"John", "lastname":"Eikenberry" }`,
-    }
-    rd := rdc.NewRedisDatabase(rdc.Configuration{})
+	blobs := []string{
+		`{ "firstname":"John", "lastname":"Eikenberry" }`,
+	}
+	rd := rdc.NewRedisDatabase(rdc.Configuration{})
 
-    for _, i := range argsAsQueues() {
-        for _, j := range blobs {
-            if !rd.Command("rpush", i, j).IsOK() {
-                fmt.Printf("Push error; %v in %v\n", j, i)
-            }
-        }
-    }
+	for _, i := range argsAsQueues() {
+		for _, j := range blobs {
+			if !rd.Command("rpush", i, j).IsOK() {
+				fmt.Printf("Push error; %v in %v\n", j, i)
+			}
+		}
+	}
 }
 
 func argsAsQueues() []string {
-    flag.Parse()
-    queues := make([]string, 0, flag.NArg())
-    for i := 0; i < flag.NArg(); i++ {
-        queues = append(queues, "resque:queue:"+flag.Arg(i))
-    }
-    return queues
+	flag.Parse()
+	queues := make([]string, 0, flag.NArg())
+	for i := 0; i < flag.NArg(); i++ {
+		queues = append(queues, "resque:queue:"+flag.Arg(i))
+	}
+	return queues
 }
